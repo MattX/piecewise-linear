857184b56877f82317f4f910a6a79c20d38244a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-17 15:33:37 +0000

    Deploy MattX/piecewise-linear to github.com/MattX/piecewise-linear.git:gh-pages

---
+++ doc/implementors/piecewise_linear/trait.CoordinateType.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["piecewise_linear"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
